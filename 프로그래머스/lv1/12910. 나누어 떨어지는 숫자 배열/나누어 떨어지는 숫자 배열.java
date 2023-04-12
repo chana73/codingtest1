10bb51f86a383887ca8be3b41f607b0b3e970830
chana73 <127104678+chana73@users.noreply.github.com>
2023-04-13 01:15:57 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.38 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        // arr = { 9, 7 }, 5
-        // divisor로 나눌 수 있는 지 확인 -> for문
-        // Arraylist 에 나누어지는 녀석들을 집어넣는다 // 5로 나누어지는 녀석들을 모을 거예요
 
-          ArrayList<Integer> answer = new ArrayList<>(); // 5의 배수를 모아놓을 곳
+class Solution {
+                    public int[] solution(int[] arr, int divisor) {
+                        // arr = {5, 9, 7, 10}, divisor = 5
+                        // divisor로 나눌 수 있는 지 확인 -> for문
+                        // Arraylist 에 나누어지는 녀석들을 집어넣는다 // 5로 나누어지는 녀석들을 모을 거예요
+
+                        ArrayList<Integer> answer = new ArrayList<>(); // 5의 배수를 모아놓을 곳
                         for (int i = 0; i < arr.length; i++) {
                             if (arr[i] % divisor == 0) {
                                 answer.add(arr[i]); // 5의 배수이면 집어 넣는다!
